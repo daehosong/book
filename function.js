c4f7b2e729c93ed3824166eb053251cafd4c4fea
daehosong <44633205+daehosong@users.noreply.github.com>
2022-12-30 02:21:59 +0900

    Update function.js

--- function.js
+++ function.js
@@ -9,7 +9,35 @@ const user = {
     }
 };
 
-
 user.callname("daeho");
 
 user.callage("22");
+
+//========================================================
+
+//const title = document.getElementById("title"); 
+//title.innerText = "Got you!";
+
+//console.log(title.id);
+//console.log(title.className);
+
+//====================================================
+
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+
+//=======================================================
+//3.2
+const title = document.querySelector("div.hello:first-child h1");
+// console.dir(title);
+function handleTitleClick(){
+ //   console.log("title was clicked!");
+    title.style.color="blue";
+}
+
+function handleMouseEnter(){
+console.log("mouse is here!");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
